a3da4f6b125d94e1e5244c6423a2a085cdba5915
Balint Erdi <balint.erdi@gmail.com>
2009-07-14 12:57:06 +0200

    few examples and impl. code in test methods

--- 13-counting-code-lines.rb
+++ 13-counting-code-lines.rb
@@ -3,18 +3,18 @@ require 'test/unit'
 if __FILE__ == $0
   class TestCodeLineCounter < Test::Unit::TestCase
     def test_empty_line_is_comment
-      line = "   "
-      is_comment = lambda { |line| true }
+      line = "   "      
+      is_comment = lambda { |line| true if line.strip.empty? }
       assert_equal(true, is_comment.call(line))
     end
     def test_line_starting_with_double_slash_is_comment
       line = "// this is a comment"
-      is_comment = lambda { |line| true }
+      is_comment = lambda { |line| true if line =~ /^\s*\/\// }
       assert_equal(true, is_comment.call(line))
     end
-    def test_line_not_having_after_a_closing_comment_is_comment
+    def test_line_not_having_nonwhitespace_after_a_closing_comment_is_comment
       line = " end of a multi-line comment */"
-      is_comment = lambda { |line| true }
+      is_comment = lambda { |line| true if line =~ /\*\/\s*$/ }
       assert_equal(true, is_comment.call(line))
     end
   end
