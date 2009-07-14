e84bad774771ad2fc75d9cef8cfd0f891277c3d0
Balint Erdi <balint.erdi@gmail.com>
2009-07-14 19:34:19 +0200

    more complicated examples, still writing implementaion in test cases

--- 13-counting-code-lines.rb
+++ 13-counting-code-lines.rb
@@ -17,5 +17,47 @@ if __FILE__ == $0
       is_comment = lambda { |line| true if line =~ /\*\/\s*$/ }
       assert_equal(true, is_comment.call(line))
     end
+    def test_not_empty_line_when_not_in_multiline_comment_is_not_comment
+      line = "x = 2"
+      in_multiline_comment = false
+      is_comment = lambda { |line| in_multiline_comment || !!(line =~ /^\s*[\/\/|\/\*]/) }
+      assert_equal(false, is_comment.call(line))
+    end
+    def test_when_in_multiline_comment_and_the_multiline_comment_sign_is_not_closed_anything_is_comment
+      line = "x = 2"
+      in_multiline_comment = true
+      is_comment = lambda { |line| in_multiline_comment && line != /\*\// }
+      assert_equal(true, is_comment.call(line))
+    end
+
+    def test_when_in_multiline_comment_and_the_multiline_comment_is_closed_but_there_is_nothing_after_is_comment
+      line = "then the funcion returns nil */"
+      in_multiline_comment = true
+      is_comment = lambda { |line| in_multiline_comment && !!(line =~ /\*\/\s*$/) }
+      assert_equal(true, is_comment.call(line))
+    end
+    
+    def test_when_in_multiline_comment_and_the_multiline_comment_is_closed_and_a_comment_until_the_end_of_line_starts_it_is_comment
+      # line = "then the funcion returns nil */ x = 2"
+      line = "then the funcion returns nil */ // e.g x = 2"
+      in_multiline_comment = true
+      is_comment = lambda { |line| in_multiline_comment && !!(line =~ /\*\/\s*\/\/.*$/)  }
+      assert_equal(true, is_comment.call(line))      
+    end
+    
+    def test_when_in_multiline_comment_and_the_multiline_comment_is_closed_and_a_another_multiline_comment_starts_is_comment
+      line = "then the funcion returns nil */ /* e.g x = 2"
+      in_multiline_comment = true
+      is_comment = lambda { |line| in_multiline_comment && !!(line =~ /\*\/\s*\/\*.*$/)  }
+      assert_equal(true, is_comment.call(line))      
+    end
+    
+    def test_when_in_multiline_comment_and_the_multiline_comment_is_closed_and_there_is_a_non_comment_afterwards_is_not_comment
+      line = "then the funcion returns nil */ x = 2"
+      in_multiline_comment = true
+      is_comment = lambda { |line| in_multiline_comment && !!(line =~ /\*\/\s*[\/\/|\/\*].*$/)  }
+      assert_equal(false, is_comment.call(line))      
+    end
+    
   end
 end
