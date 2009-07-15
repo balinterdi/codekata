50e43a1927a7a43c34f702c7c9000562877fa09e
Balint Erdi <balint.erdi@gmail.com>
2009-07-15 17:18:46 +0200

    easy cases refactored into method in test class

--- 13-counting-code-lines.rb
+++ 13-counting-code-lines.rb
@@ -1,22 +1,36 @@
 require 'test/unit'
+require "mocha"
 
 if __FILE__ == $0
   class TestCodeLineCounter < Test::Unit::TestCase
-    def test_empty_line_is_comment
-      line = "   "      
-      is_comment = lambda { |line| true if line.strip.empty? }
-      assert_equal(true, is_comment.call(line))
+    def is_comment(line)
+      line.strip!
+      return true if line.empty?
+      case line
+      when /^\/\//
+        true
+      when /\*\/$/
+        true
+      else
+        false
+      end
     end
+
+    def test_empty_line_is_comment
+      line = "   "
+      assert_equal(true, is_comment(line))
+    end
+
     def test_line_starting_with_double_slash_is_comment
       line = "// this is a comment"
-      is_comment = lambda { |line| true if line =~ /^\s*\/\// }
-      assert_equal(true, is_comment.call(line))
+      assert_equal(true, is_comment(line))
     end
-    def test_line_not_having_nonwhitespace_after_a_closing_comment_is_comment
+
+    def test_line_not_having_nonwhitespace_after_a_closing_multiline_comment_is_comment
       line = " end of a multi-line comment */"
-      is_comment = lambda { |line| true if line =~ /\*\/\s*$/ }
-      assert_equal(true, is_comment.call(line))
+      assert_equal(true, is_comment(line))
     end
+
     def test_not_empty_line_when_not_in_multiline_comment_is_not_comment
       line = "x = 2"
       in_multiline_comment = false
@@ -36,28 +50,28 @@ if __FILE__ == $0
       is_comment = lambda { |line| in_multiline_comment && !!(line =~ /\*\/\s*$/) }
       assert_equal(true, is_comment.call(line))
     end
-    
+
     def test_when_in_multiline_comment_and_the_multiline_comment_is_closed_and_a_comment_until_the_end_of_line_starts_it_is_comment
       # line = "then the funcion returns nil */ x = 2"
       line = "then the funcion returns nil */ // e.g x = 2"
       in_multiline_comment = true
       is_comment = lambda { |line| in_multiline_comment && !!(line =~ /\*\/\s*\/\/.*$/)  }
-      assert_equal(true, is_comment.call(line))      
+      assert_equal(true, is_comment.call(line))
     end
-    
+
     def test_when_in_multiline_comment_and_the_multiline_comment_is_closed_and_a_another_multiline_comment_starts_is_comment
       line = "then the funcion returns nil */ /* e.g x = 2"
       in_multiline_comment = true
       is_comment = lambda { |line| in_multiline_comment && !!(line =~ /\*\/\s*\/\*.*$/)  }
-      assert_equal(true, is_comment.call(line))      
+      assert_equal(true, is_comment.call(line))
     end
-    
+
     def test_when_in_multiline_comment_and_the_multiline_comment_is_closed_and_there_is_a_non_comment_afterwards_is_not_comment
       line = "then the funcion returns nil */ x = 2"
       in_multiline_comment = true
       is_comment = lambda { |line| in_multiline_comment && !!(line =~ /\*\/\s*[\/\/|\/\*].*$/)  }
-      assert_equal(false, is_comment.call(line))      
+      assert_equal(false, is_comment.call(line))
     end
-    
+
   end
 end
