f5d48882f588b5ec5f8f9ba81b2051f70a675e1d
Balint Erdi <balint.erdi@gmail.com>
2009-07-16 10:56:58 +0200

    is_comment renamed to comment?

--- 13-counting-code-lines.rb
+++ 13-counting-code-lines.rb
@@ -2,12 +2,12 @@ require 'test/unit'
 
 class CodeLineCounter
   class << self
-    def is_comment(line, in_multiline_comment)
+    def comment?(line, in_multiline_comment)
       line.strip!
       return [true, in_multiline_comment] if line.empty?
       if in_multiline_comment
         if line =~ /\*\/(.*)/
-          is_comment($1, false)
+          comment?($1, false)
         else
           [true, true]
         end
@@ -15,7 +15,7 @@ class CodeLineCounter
         if line =~ /^\/\//
           [true, false]
         elsif line =~ /^\/\*(.*)/
-           is_comment($1, true)
+           comment?($1, true)
         else
            [false, false]
         end
@@ -27,7 +27,7 @@ class CodeLineCounter
       in_multiline_comment = false
       text.inject(0) do |loc, line|
         # puts "XXX Line: #{line} In multiline comment? #{in_multiline_comment} LOC: #{loc}"
-        result, in_multiline_comment = is_comment(line, in_multiline_comment)
+        result, in_multiline_comment = comment?(line, in_multiline_comment)
         loc + (result ? 0 : 1)
       end
     end
@@ -38,56 +38,56 @@ if __FILE__ == $0
   class TestCodeLineCounter < Test::Unit::TestCase
     def test_empty_line_is_comment
       line = "   "
-      assert_equal(true, CodeLineCounter.is_comment(line, false)[0])
+      assert_equal(true, CodeLineCounter.comment?(line, false)[0])
     end
 
     def test_line_starting_with_double_slash_is_comment
       line = "// this is a comment"
-      assert_equal(true, CodeLineCounter.is_comment(line, false)[0])
+      assert_equal(true, CodeLineCounter.comment?(line, false)[0])
     end
 
     def test_line_which_has_something_before_the_comment_sign_is_not_comment
       line = "x = x.strip // to make sure no space lingers"
-      assert_equal(false, CodeLineCounter.is_comment(line, false)[0])
+      assert_equal(false, CodeLineCounter.comment?(line, false)[0])
     end
 
     def test_when_not_in_multiline_a_multiline_line_containing_a_closing_multiline_comment_sign_is_not_comment
       line = " x */ 2"
-      assert_equal(false, CodeLineCounter.is_comment(line, false)[0])
+      assert_equal(false, CodeLineCounter.comment?(line, false)[0])
     end
 
     def test_not_empty_line_when_not_in_multiline_comment_is_not_comment
       line = "x = 2"
-      assert_equal(false, CodeLineCounter.is_comment(line, false)[0])
+      assert_equal(false, CodeLineCounter.comment?(line, false)[0])
     end
     def test_when_in_multiline_comment_and_the_multiline_comment_sign_is_not_closed_anything_is_comment
       line = "x = 2"
-      assert_equal(true, CodeLineCounter.is_comment(line, true)[0])
+      assert_equal(true, CodeLineCounter.comment?(line, true)[0])
     end
 
     def test_when_in_multiline_comment_and_the_multiline_comment_is_closed_but_there_is_nothing_after_is_comment
       line = "then the funcion returns nil */"
-      assert_equal(true, CodeLineCounter.is_comment(line, true)[0])
+      assert_equal(true, CodeLineCounter.comment?(line, true)[0])
     end
 
     def test_when_in_multiline_comment_and_the_multiline_comment_is_closed_and_a_comment_until_the_end_of_line_starts_it_is_comment
       line = "then the funcion returns nil */ // e.g x = 2"
-      assert_equal(true, CodeLineCounter.is_comment(line, true)[0])
+      assert_equal(true, CodeLineCounter.comment?(line, true)[0])
     end
 
     def test_when_in_multiline_comment_and_the_multiline_comment_is_closed_and_a_another_multiline_comment_starts_is_comment
       line = "then the funcion returns nil */ /* e.g x = 2"
-      assert_equal(true, CodeLineCounter.is_comment(line, true)[0])
+      assert_equal(true, CodeLineCounter.comment?(line, true)[0])
     end
 
     def test_when_in_multiline_comment_and_the_multiline_comment_is_closed_and_there_is_a_non_comment_afterwards_is_not_comment
       line = "then the funcion returns nil */ x = 2"
-      assert_equal(false, CodeLineCounter.is_comment(line, true)[0])
+      assert_equal(false, CodeLineCounter.comment?(line, true)[0])
     end
 
     def test_when_in_multiline_comment_and_the_multine_is_closed_and_there_is_code_after_and_another_starts_is_not_comment
       line = "then the function returns nil */ x = 2 /*"
-      assert_equal(false, CodeLineCounter.is_comment(line, true)[0])
+      assert_equal(false, CodeLineCounter.comment?(line, true)[0])
     end
 
     # ----
